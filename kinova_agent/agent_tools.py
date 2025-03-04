06729bd2b13b0f5b383c2c61b6fd4565f6333323
Abhishek <deshmukhabhi02@gmail.com>
2025-03-04 14:00:03 +0100

    Update #19: Added more tools

--- kinova_agent/agent_tools.py
+++ kinova_agent/agent_tools.py
@@ -15,8 +15,7 @@ def move_to_home_pose ():
     """
     Moves the Robot arm coordinates for home pose
     """
-    # home_pose_coordinates = [1.0, 0.0, 0.5, 0.5, 0.3, -0.5, 0.5, 0.5]
-    home_pose_coordinates = [0.0, 0.0, -0.8, -3.15, -2.0, 0.0, -1.2, 1.55]
+    home_pose_coordinates = [0.0, 0.0, -0.8650, -3.15, -2.13, 0.006, -1.2, 1.55]
     publish_to(type_name=COORDINATE_TYPE, topic_name=COORDINATE_TOPIC, coordinates=home_pose_coordinates)
        
 @tool
@@ -125,7 +124,135 @@ def rotate_the_gripper_anticlockwise():
     """
     publish_to(type_name=COORDINATE_TYPE, topic_name=COORDINATE_TOPIC, coordinates=get_direction_coordinates("anticlockwise"))
 
+
+@tool
+def launch_vision ():
+    """
+    Starts the vision module of the Robot
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "vision.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Vision Module launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
     
+    
+@tool
+def launch_the_fake_robot():
+    """
+    Launches the Fake robot in simulation
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "fake_robot_launch.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Fake Robot launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+        
+        
+@tool
+def launch_the_robot():
+    """
+    Launches and connects to the real robot
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "robot_launch.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Robot launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+        
+ 
+@tool
+def launch_the_listener():
+    """
+    Launches the moveit interface to process basic movememnts of the robot
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "agent_listener.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Listener launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+
+
+@tool
+def launch_contact_graspnet():
+    """
+    Launches the contact graspnet for grasp pose estimation
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/contact-graspnet/docker_run"
+    script_path = os.path.join(script_dir, "contact_graspnet.sh")  
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Vision Module launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+         
+         
+@tool
+def pick_the_objects_in_front():
+    """
+    Predicts the best grasp pose for the objects in the field of view
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "kinova_ops.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Kinova Ops Module launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+        
+        
+@tool
+def launch_the_task_constructor ():
+    """
+    Runs the stack for generating grasps and picking up objects in field of view
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "pick_n_place.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Pick and Place node launched in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)  
+
+
+@tool 
+def execute_the_motion_plan ():
+    """
+    Executes the motion plan by executing bash scripts
+    """
+    script_dir = "/home/abhishek/workspaces/kinova_ws/src/kinova-ros2/docker_run"
+    script_path = os.path.join(script_dir, "motion_plan.sh")
+    try: 
+        subprocess.Popen(["terminator", 
+                          "-e", 
+                          f"bash -c 'source /opt/ros/humble/setup.bash && {script_path}; exec bash'"])
+        print("Motion Plan executed in a new terminal")
+    except Exception as e:
+        print("Error launching the terminator: ", e)
+   
+   
 @tool 
 def stop():
     """
@@ -144,22 +271,25 @@ def stop():
                 os.kill(int(pid), signal.SIGTERM)
         except subprocess.CalledProcessError:
             pass
-
-@tool 
-def execute_pick_and_place_routine ():
-    """
-    Executes the pick and place routine by executing bash scripts
-    """
-
+ 
+     
 def get_tools () -> list[Tool]:
     return [rotate_the_gripper_anticlockwise,
             rotate_the_gripper_clockwise,
+            launch_the_task_constructor,
+            pick_the_objects_in_front,
+            execute_the_motion_plan,
+            launch_contact_graspnet,
             describe_what_you_see,
-            move_to_zero_position,
+            move_to_zero_position, 
+            launch_the_fake_robot,
             move_to_retract_pose,
+            launch_the_listener,
             move_to_home_pose,
+            launch_the_robot,
             move_downwards,
             move_backward,
+            launch_vision,
             close_gripper,
             open_gripper, 
             move_forward, 
--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from setuptools import find_packages, setup
+import glob
 
 package_name = 'kinova_agent'
 
@@ -10,6 +11,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        ('share/' + package_name + '/scripts', glob.glob(package_name + '/scripts/*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
