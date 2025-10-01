311e79f841da550a5ae9426777a9aeb1b2cf1c31
Abhishek <deshmukhabhi02@gmail.com>
2025-10-01 11:31:11 +0200

    #30: Added Files

---
+++ docker_run/ollama_server.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
+
+xhost +
+docker run \
+		-it \
+		--rm \
+		--net=host \
+		--pid=host \
+		--ipc=host \
+		--privileged \
+        --gpus all \
+        --runtime=nvidia \
+		-v /dev:/dev \
+		-v $HOME/.ros/log:/.ros/log \
+		-v /tmp/.X11-unix:/tmp/.X11-unix \
+		--env RMW_IMPLEMENTATION=${RMW_IMPLEMENTATION} \
+		--env DISPLAY=$DISPLAY \
+        --name server \
+        agent:latest \
+		/entrypoint_scripts/entrypoint_ollama_server.sh \
